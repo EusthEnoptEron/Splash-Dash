009aa1ee7f97643dd00bce60f72046cdf7a5b047
Simon Meer <simon.meer@bluewin.ch>
2015-05-23 14:27:11 +0200

    Tweak camera

--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class SmoothFollow : MonoBehaviour {
 	public Transform target;            // The target we are following
@@ -12,13 +14,37 @@ public class SmoothFollow : MonoBehaviour {
 		// Early out if we don't have a target
 		if (!target) return;
 		
-		// Calculate the current rotation angles
-		float wantedRotationAngle = target.eulerAngles.y;
-		float wantedHeight = target.position.y + height;
-		
 		float currentRotationAngle = transform.eulerAngles.y;
 		float currentHeight = transform.position.y;
-		
+
+        // Calculate the current rotation angles
+        float wantedHeight = target.position.y + height;
+
+        RaycastHit hit;
+        int i = 0;
+        float wantedRotationAngle = RotationAngles.FirstOrDefault(
+            angle =>
+            {
+                var pos = PositionFromRotationAngle(angle, wantedHeight);
+                var direction = (target.position - pos).normalized;
+                pos -= direction * 2;
+                //Debug.DrawRay(pos - direction * 5, direction, Color.green);
+
+                if (Physics.Raycast(pos, direction, out hit))
+                {
+                    //Debug.LogFormat("{0} ({1})", hit.collider.name, i++);
+                    return hit.collider.transform.IsChildOf(target);
+
+                }
+                return false;
+            }
+        );
+        if (wantedRotationAngle == default(float))
+        {
+            wantedRotationAngle = RotationAngles.First();
+        }
+        //Debug.Log(wantedRotationAngle);
+
 		// Damp the rotation around the y-axis
 		currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, 
 		                                        wantedRotationAngle, 
@@ -43,4 +69,43 @@ public class SmoothFollow : MonoBehaviour {
 		// Always look at the target
 		transform.LookAt(target);
 	}
+
+    IEnumerable<float> RotationAngles
+    {
+        get
+        {
+            float targetAngle = target.eulerAngles.y;
+            yield return targetAngle;
+
+            for (int i = 5; i < 180; i += 50)
+            {
+                yield return targetAngle + i;
+                yield return targetAngle - i;
+            }
+
+            //for (int i = 5; i < 180; i += 10)
+            //{
+            //    yield return targetAngle - i;
+            //    //yield return targetAngle - i;
+            //}
+            
+        }
+    }
+
+    private Vector3 PositionFromRotationAngle(float rotationAngle, float height)
+    {
+        // Convert the angle into a rotation around the y-axis
+        Quaternion currentRotation = Quaternion.Euler(0, rotationAngle, 0);
+
+        // Set the position of the camera on the x-z plane to:
+        // distance meters behind the target
+        var position = target.position;
+        position -= currentRotation * Vector3.forward * distance;
+
+        // Set the height of the camera
+        position = new Vector3(position.x,
+                            height,
+                            position.z);
+        return position;
+    }
 }
